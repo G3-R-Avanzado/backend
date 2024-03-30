24d58cc685ef3c83f32361c13a97d5f27e71236b
AgustinSanchez1 <agustinsanchezdev@gmail.com>
2024-03-30 17:31:15 -0300

    actualizacion cadena de conexion a db prod

--- src/db.js
+++ src/db.js
@@ -4,9 +4,8 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    //const BASE_URL = process.env.CONNECT_URL;
-    const BASE_URL = `${process.env.PORTDB}/${process.env.DB}`;
-    console.log(BASE_URL);
+    const BASE_URL = process.env.CONNECT_URL;
+    //const BASE_URL = `${process.env.PORTDB}/${process.env.DB}`;
     await mongoose.connect(BASE_URL);
     console.log("db connected");
   } catch (error) {
